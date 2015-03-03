2f1f1c60c19358468f6e5474b8be12d8cde7274f
ziotoss <ziotoss@gmail.com>
2015-03-03 11:03:46 +0900

    Implement thread operation for multicore PC
    Multi-thread operation of retrieving song request data implemented.

--- src/RadioEpisode/SearchRequestSong.java
+++ src/RadioEpisode/SearchRequestSong.java
@@ -15,12 +15,18 @@ import java.util.Scanner;
 import DataTypes.EpisodeObject;
 import DataTypes.SongObject;
 
-
-public class SearchRequestSong {
-
-	private final static String DATAPATH = "D:/Data/RadioEpisode/";
+class SearchRequestSongThread extends Thread {
+	private Thread t;
+	private String startIdx;
+	private ArrayList<EpisodeObject> epiList;
 	private final String OUTPUTPATH = "D:/Data/RadioEpisode/sbs_request_songs2/";
-	private Connection kpopConnect = null;
+	private static Connection kpopConnect = null;
+	
+	SearchRequestSongThread(String idx, ArrayList<EpisodeObject> epiL) {
+		startIdx = idx;
+		epiList = epiL;
+		System.out.println("Creating thread for index " + startIdx);
+	}
 	
 	private void connectToDb() {
 		try {
@@ -33,12 +39,12 @@ public class SearchRequestSong {
 			e.printStackTrace();
 		}
 	}
-
+	
 	private ArrayList<SongObject> retrieveSongInfo() {
 		System.out.println("Retrieving song info from Db.");
 		ArrayList<SongObject> songInfo = new ArrayList<SongObject>();
 		try {
-			PreparedStatement select = kpopConnect.prepareStatement("SELECT * FROM kpop_archive LIMIT 120000, 10000");
+			PreparedStatement select = kpopConnect.prepareStatement("SELECT * FROM kpop_archive LIMIT " + startIdx + ", 10000");
 			ResultSet rs = select.executeQuery();
 			
 			while(rs.next()) {
@@ -68,9 +74,7 @@ public class SearchRequestSong {
 		return songInfo;
 	}
 
-	private void retrieveRequestSong(ArrayList<SongObject> songObj) {
-
-		ArrayList<EpisodeObject> epiList = retrieveEpisodeInfo();
+	private void retrieveRequestSong(ArrayList<SongObject> songObj, ArrayList<EpisodeObject> epiList) {
 		
 		for(SongObject rtObj : songObj) {
 			System.out.println("Processing artist = " + rtObj.getArtist() + " title = " + rtObj.getTitle());
@@ -82,40 +86,6 @@ public class SearchRequestSong {
 			} else
 				System.out.println("Already processed.");
 		}
-	}
-	
-	private ArrayList<EpisodeObject> retrieveEpisodeInfo() {
-		System.out.println("Storing episodes into arraylist.");
-		String[] programs = {"sbs12pm_201412", "sbs2pm_201412", "sbs4pm_201412", "sbs6pm_201412", "sbs8pm_201412", "sbs10pm_201412", "sbs12am_201412"};
-		ArrayList<EpisodeObject> epiObjList = new ArrayList<EpisodeObject>();
-		
-		for(String program : programs) {
-
-			File root = new File(DATAPATH + program);
-			
-			for(File f : root.listFiles()) {
-				EpisodeObject epiObj = new EpisodeObject();
-				epiObj.setEpisodeId(f.getAbsolutePath());
-				
-				try {
-					Scanner scanner = new Scanner(new FileReader(f));
-					ArrayList<String> sentences = new ArrayList<String>();
-
-					while(scanner.hasNext()) {
-						String line = scanner.nextLine().trim();
-						if(!line.equals(""))
-							sentences.add(line);
-					}
-					scanner.close();
-					epiObj.setSentences(sentences);
-					epiObjList.add(epiObj);
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return epiObjList;
 	}
 	
 	private ArrayList<String> searchRequestSongs(String artist, String title, ArrayList<EpisodeObject> epiList) {
@@ -197,7 +167,7 @@ public class SearchRequestSong {
 			}
 		}
 		
-		File outFile = new File(outDir.getAbsolutePath() + "/" + artist + "_" + title.replaceAll("\\p{Punct}", "").trim() + ".txt");
+		File outFile = new File(outDir.getAbsolutePath() + "/" + artist.replaceAll("\\p{Punct}", "").trim() + "_" + title.replaceAll("\\p{Punct}", "").trim() + ".txt");
 		String NL = System.getProperty("line.separator");
 		try {
 			writer = new BufferedWriter(new FileWriter(outFile));
@@ -222,12 +192,81 @@ public class SearchRequestSong {
 		} catch(Exception e) {}
 	}
 	
+	public void run() {
+		connectToDb();
+		ArrayList<SongObject> songs = retrieveSongInfo();
+		retrieveRequestSong(songs, epiList);
+		close();
+	}
+	
+	public void start() {
+		if (t == null) {
+			t = new Thread(this, startIdx);
+			t.start();
+		}
+	}
+}
+
+
+class SearchRequestSong {
+
+	private final static String DATAPATH = "D:/Data/RadioEpisode/";
+	
+	private ArrayList<EpisodeObject> retrieveEpisodeInfo() {
+		System.out.println("Storing episodes into arraylist.");
+		String[] programs = {"sbs12pm_201412", "sbs2pm_201412", "sbs4pm_201412", "sbs6pm_201412", "sbs8pm_201412", "sbs10pm_201412", "sbs12am_201412"};
+		ArrayList<EpisodeObject> epiObjList = new ArrayList<EpisodeObject>();
+		
+		for(String program : programs) {
+
+			File root = new File(DATAPATH + program);
+			
+			for(File f : root.listFiles()) {
+				EpisodeObject epiObj = new EpisodeObject();
+				epiObj.setEpisodeId(f.getAbsolutePath());
+				
+				try {
+					Scanner scanner = new Scanner(new FileReader(f));
+					ArrayList<String> sentences = new ArrayList<String>();
+
+					while(scanner.hasNext()) {
+						String line = scanner.nextLine().trim();
+						if(!line.equals(""))
+							sentences.add(line);
+					}
+					scanner.close();
+					epiObj.setSentences(sentences);
+					epiObjList.add(epiObj);
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return epiObjList;
+	}
+
 	public static void main(String[] args) {
 		SearchRequestSong dao = new SearchRequestSong();
-		dao.connectToDb();
-		ArrayList<SongObject> songs = dao.retrieveSongInfo();
-		dao.retrieveRequestSong(songs);		
-		dao.close();
+		//dao.connectToDb();
+		
+		ArrayList<EpisodeObject> epiObj = dao.retrieveEpisodeInfo();
+		
+		SearchRequestSongThread T1 = new SearchRequestSongThread("120000", epiObj);
+		T1.start();
+		
+		SearchRequestSongThread T2 = new SearchRequestSongThread("130000", epiObj);
+		T2.start();
+		
+		SearchRequestSongThread T3 = new SearchRequestSongThread("140000", epiObj);
+		T3.start();
+		
+		SearchRequestSongThread T4 = new SearchRequestSongThread("150000", epiObj);
+		T4.start();
+		//dao1.connectToDb();
+		//ArrayList<SongObject> songs = dao.retrieveSongInfo();
+		//dao.retrieveRequestSong(songs);		
+		//dao.close();
 	}
 
 }
